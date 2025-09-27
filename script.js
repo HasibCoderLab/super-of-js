80dda5ec410a0fb369d2d34fe792f75b60079158
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-27 17:50:47 +0600

    update 2nd example

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // ===========|Example 01|==========
-class Person {
+// class Person {
   
+//     eat() {
+//         console.log("Just eat and Eat");
+
+//     };
+//     work() {
+//         console.log("Lazy Man");
+
+//     };
+// }
+
+// class Engineer extends Person {
+//     eat() {
+//         console.log("All halah Foods need");
+
+//     };
+//     work(){
+//         console.log("Creative think");
+        
+//     };
+// }
+
+// let engObj = new Person();
+// engObj.work()
+
+// ===========|Example 02|==========
+class Person {
+  constructor(name){
+    console.log("Enter Parents");
+    
+    this.name = name
+  }
     eat() {
         console.log("Just eat and Eat");
 
@@ -12,6 +43,14 @@ class Person {
 }
 
 class Engineer extends Person {
+    constructor(branch){
+        console.log("Enter Child");
+        
+        super();
+        this.branch = branch;
+        console.log("Exit");
+        
+    }
     eat() {
         console.log("All halah Foods need");
 
@@ -21,3 +60,7 @@ class Engineer extends Person {
         
     };
 }
+
+let EngObj = new Person("CSE")
+// EngObj.name()
+console.log(EngObj);

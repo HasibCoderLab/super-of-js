262a2e8035da2538b6cc8cb71f15e6bd85d996b4
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-27 17:37:50 +0600

    writing a code in js ffile

---
+++ script.js
@@ -0,0 +1,18 @@
+class Person {
+
+    eat() {
+        console.log("Just eat and Eat");
+
+    };
+    work(){
+        console.log("Lazy Man");
+        
+    };
+}
+
+class Engineer extends Person {
+eat(){
+    console.log("All halah Foods need");
+    
+};
+}
